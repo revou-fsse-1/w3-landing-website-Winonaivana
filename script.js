14805aa45b07725472a2bb5f9ee42cf67a4be25a
winona ivana <winonaivana@gmail.com>
2023-02-01 14:22:19 +0000

    Initial Code

---
+++ script.js
@@ -0,0 +1,20 @@
+let coca = document.getElementById("coca");
+let red = document.getElementById("red");
+let text = document.getElementById("text");
+let btn = document.getElementById("btn");
+let yas = document.getElementById("yas");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+
+  coca.style.top = value * 1.05 + "px";
+  red.style.top = value * 0.5 + "px";
+  text.style.marginRight = value * 2 + "px";
+  text.style.marginTop = value * 1.5 + "px";
+  btn.style.marginTop = value * 1.5 + "px";
+});
+
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
